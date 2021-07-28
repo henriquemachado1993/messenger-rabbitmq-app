f7e040d60b7f3d24470f5540ba7882e32072539f
Henrique Machado <henriquemachado1993@gmail.com>
2021-07-27 22:58:29 -0300

    Primeiro commit

---
+++ Consumidor/Consumidor/Program.cs
@@ -0,0 +1,44 @@
+﻿using Consumidor.Model;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Consumidor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                // Criando a fila
+                channel.QueueDeclare(queue: "Messages",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+                                
+                Console.WriteLine("Esperando pela mensagem...");
+
+                // Consumindo a mensagem
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (sender, ea) =>
+                {
+                    var contentArray = ea.Body.ToArray();
+                    var contentString = Encoding.UTF8.GetString(contentArray);                    
+                    Console.WriteLine(" Recebido {0}", contentString);
+                };
+
+                channel.BasicConsume(queue: "Messages",
+                                     autoAck: true,
+                                     consumer: consumer);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Produtor/Produtor/Program.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using Produtor.Model;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Produtor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                // Cria a fila
+                channel.QueueDeclare(queue: "Messages",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+                int count = 0;
+                while (true)
+                {
+                    // Monta a mensagem
+                    var message = $"{count++} Guid: {Guid.NewGuid()}";
+
+                    // Transforma a mensagem em bytes
+                    string stringfiedmessage = JsonConvert.SerializeObject(message);
+                    var body = Encoding.UTF8.GetBytes(stringfiedmessage);
+
+                    // Publica a mensagem na fila
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "Messages",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" Enviado {0}", message);
+                    Thread.Sleep(200);
+                }                
+            }
+        }
+    }
+}
